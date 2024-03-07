7c7e18b08474837ac4a8629780be9f759ad2e5f1
Mihail <m.misha0307@gmail.com>
2024-03-07 21:34:45 +0200

    first commit

---
+++ src/stories/animations/animation_2/index.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+const Animation = () =>  {
+    return (
+        <div className="container">
+            <div className="bitcoin"></div>
+        </div>
+    );
+}
+
+export {Animation};
---
+++ src/stories/animations/animation_3/index.stories.ts
@@ -0,0 +1,16 @@
+import {Meta, StoryObj} from "@storybook/react";
+import {Animation} from "./index.tsx";
+
+const meta = {
+    title: "animations/animation_3",
+    component: Animation
+} satisfies Meta<typeof Animation>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Story: Story = {
+    name: "Default",
+    args: {}
+};
